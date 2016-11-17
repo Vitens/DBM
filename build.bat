de9c318aaf3870ff6172ba7cd55bd556a91a97d0
Johan Fitié <jfitie@gmail.com>
2016-11-17 20:00:31 +0100

    Update scripts to clean build directory

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
 @echo off
+%~d0
+cd %~dp0
+
+call clean.bat
 set IncludeFiles=src\dbm\DBMConstants.vb src\dbm\DBMStatistics.vb src\dbm\DBMMath.vb src\dbm\DBMCachedValue.vb src\dbm\DBMPoint.vb src\dbm\dbm.vb
 "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\Vbc.exe" /out:build\DBMUnitTests-offline.exe /define:OfflineUnitTests=True %IncludeFiles% test\unit\DBMUnitTests.vb
 if exist "%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDK.dll" "%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\vbc.exe" /reference:"%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDK.dll","%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDKCommon.dll" /out:build\DBMUnitTests.exe %IncludeFiles% test\unit\DBMUnitTests.vb
