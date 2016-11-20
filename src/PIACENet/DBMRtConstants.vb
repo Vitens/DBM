9ef4013f91de9e487c422ee188a1604c6ff66d2a
Johan Fitié <jfitie@gmail.com>
2016-11-20 10:49:33 +0100

    Add NormSInv function to DBMMath

--- src/PIACENet/DBMRtConstants.vb
+++ src/PIACENet/DBMRtConstants.vb
@@ -8,7 +8,7 @@ Option Strict
 
 Public Class DBMRtConstants
 
-    Public Const MaxCalculationAge As Integer =             30 ' days
-    Public Const CalculationDelay As Integer =              10 ' seconds
+    Public Const MaxCalculationAge As Integer=              30 ' days
+    Public Const CalculationDelay As Integer=               10 ' seconds
 
 End Class
--- src/dbm/DBMMath.vb
+++ src/dbm/DBMMath.vb
@@ -8,6 +8,30 @@ Option Strict
 
 Public Class DBMMath
 
+    Private Function NormSInv(ByVal p As Double) As Double
+        Const a1=-39.6968302866538,a2=220.946098424521,a3=-275.928510446969
+        Const a4=138.357751867269,a5=-30.6647980661472,a6=2.50662827745924
+        Const b1=-54.4760987982241,b2=161.585836858041,b3=-155.698979859887
+        Const b4=66.8013118877197,b5=-13.2806815528857,c1=-7.78489400243029E-03
+        Const c2=-0.322396458041136,c3=-2.40075827716184,c4=-2.54973253934373
+        Const c5=4.37466414146497,c6=2.93816398269878,d1=7.78469570904146E-03
+        Const d2=0.32246712907004,d3=2.445134137143,d4=3.75440866190742
+        Const p_low=0.02425,p_high=1-p_low
+        Dim q,r As Double
+        If p<p_low Then
+            q=Math.Sqrt(-2*Math.Log(p))
+            NormSInv=(((((c1*q+c2)*q+c3)*q+c4)*q+c5)*q+c6)/((((d1*q+d2)*q+d3)*q+d4)*q+1)
+        ElseIf p<=p_high Then
+            q=p-0.5
+            r=q*q
+            NormSInv=(((((a1*r+a2)*r+a3)*r+a4)*r+a5)*r+a6)*q/(((((b1*r+b2)*r+b3)*r+b4)*r+b5)*r+1)
+        Else
+            q=Math.Sqrt(-2*Math.Log(1-p))
+            NormSInv=-(((((c1*q+c2)*q+c3)*q+c4)*q+c5)*q+c6)/((((d1*q+d2)*q+d3)*q+d4)*q+1)
+        End If
+        Return NormSInv
+    End Function
+
     Private Function MeanAbsDevScaleFactor As Double ' Scale factor k
         Return 1.253314137316 ' SQRT(PI()/2)
     End Function
@@ -69,7 +93,7 @@ Public Class DBMMath
             Case 29
                 MedianAbsDevScaleFactor=1.463372853805
             Case >=30
-                MedianAbsDevScaleFactor=1.482602218506 ' n>=30 Standard normal distribution: 1/NORM.S.INV(75%)
+                MedianAbsDevScaleFactor=1/NormSInv(0.75) ' n>=30 Standard normal distribution: 1/NORM.S.INV(75%)
         End Select
         Return MedianAbsDevScaleFactor
     End Function
@@ -131,7 +155,7 @@ Public Class DBMMath
             Case 29
                 ControlLimitRejectionCriterion=2.763262455461
             Case >=30
-                ControlLimitRejectionCriterion=2.575829303549 ' n>=30 Standard normal distribution: NORM.S.INV(1-1%/2) (P=99%)
+                ControlLimitRejectionCriterion=NormSInv((DBMConstants.ConfidenceInterval+1)/2) ' n>=30 Standard normal distribution: NORM.S.INV(1-1%/2) (P=99%)
         End Select
         Return ControlLimitRejectionCriterion
     End Function
