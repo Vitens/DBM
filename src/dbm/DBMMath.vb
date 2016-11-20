075f3e5a231b3f2b44798e46d082795efb82abe2
Johan Fitié <jfitie@gmail.com>
2016-11-20 15:30:35 +0100

    Update MedianAbsDevScaleFactor to make use of TInv2T and NormSInv functions

--- src/dbm/DBMMath.vb
+++ src/dbm/DBMMath.vb
@@ -64,78 +64,24 @@ Public Class DBMMath
     End Function
 
     Private Function MeanAbsDevScaleFactor As Double ' Scale factor k
-        Return 1.253314137316 ' SQRT(PI()/2)
+        Return Math.Sqrt(Math.PI/2)
     End Function
 
     Private Function MedianAbsDevScaleFactor(ByVal n As Integer) As Double ' Scale factor k
-        Select Case n
-            Case <=3
-                MedianAbsDevScaleFactor=1.224744871392 ' n<30 Student's t-distribution: 1/T.INV(75%,n-1)
-            Case 4
-                MedianAbsDevScaleFactor=1.307373551629
-            Case 5
-                MedianAbsDevScaleFactor=1.350079568894
-            Case 6
-                MedianAbsDevScaleFactor=1.376108579000
-            Case 7
-                MedianAbsDevScaleFactor=1.393615186740
-            Case 8
-                MedianAbsDevScaleFactor=1.406189357483
-            Case 9
-                MedianAbsDevScaleFactor=1.415655367895
-            Case 10
-                MedianAbsDevScaleFactor=1.423037547092
-            Case 11
-                MedianAbsDevScaleFactor=1.428955079918
-            Case 12
-                MedianAbsDevScaleFactor=1.433804142754
-            Case 13
-                MedianAbsDevScaleFactor=1.437849945108
-            Case 14
-                MedianAbsDevScaleFactor=1.441276685628
-            Case 15
-                MedianAbsDevScaleFactor=1.444216273612
-            Case 16
-                MedianAbsDevScaleFactor=1.446765645460
-            Case 17
-                MedianAbsDevScaleFactor=1.448997629771
-            Case 18
-                MedianAbsDevScaleFactor=1.450968000282
-            Case 19
-                MedianAbsDevScaleFactor=1.452720190409
-            Case 20
-                MedianAbsDevScaleFactor=1.454288526282
-            Case 21
-                MedianAbsDevScaleFactor=1.455700494239
-            Case 22
-                MedianAbsDevScaleFactor=1.456978363159
-            Case 23
-                MedianAbsDevScaleFactor=1.458140366059
-            Case 24
-                MedianAbsDevScaleFactor=1.459201574556
-            Case 25
-                MedianAbsDevScaleFactor=1.460174555449
-            Case 26
-                MedianAbsDevScaleFactor=1.461069870224
-            Case 27
-                MedianAbsDevScaleFactor=1.461896459630
-            Case 28
-                MedianAbsDevScaleFactor=1.462661943036
-            Case 29
-                MedianAbsDevScaleFactor=1.463372853805
-            Case >=30
-                MedianAbsDevScaleFactor=1/NormSInv(0.75) ' n>=30 Standard normal distribution: 1/NORM.S.INV(75%)
-        End Select
+        If n<30 Then
+            MedianAbsDevScaleFactor=1/TInv2T(1-(1-0.75)*2,n) ' n<30 Student's t-distribution
+        Else
+            MedianAbsDevScaleFactor=1/NormSInv(0.75) ' n>=30 Standard normal distribution
+        End If
         Return MedianAbsDevScaleFactor
     End Function
 
     Public Function ControlLimitRejectionCriterion(ByVal n As Integer) As Double
-        Select Case n
-            Case <30
-                ControlLimitRejectionCriterion=TInv2T(1-DBMConstants.ConfidenceInterval,Math.Max(2,n)) ' n<30 Student's t-distribution
-            Case >=30
-                ControlLimitRejectionCriterion=NormSInv((DBMConstants.ConfidenceInterval+1)/2) ' n>=30 Standard normal distribution
-        End Select
+        If n<30 Then
+            ControlLimitRejectionCriterion=TInv2T(1-DBMConstants.ConfidenceInterval,Math.Max(2,n)) ' n<30 Student's t-distribution
+        Else
+            ControlLimitRejectionCriterion=NormSInv((DBMConstants.ConfidenceInterval+1)/2) ' n>=30 Standard normal distribution
+        End If
         Return ControlLimitRejectionCriterion
     End Function
 
@@ -187,7 +133,7 @@ Public Class DBMMath
                     Data(i)=Double.NaN ' Exclude outlier
                 End If
             Else ' Use Median Absolute Deviation to detect outliers
-                If Math.Abs(Data(i)-Median)>MedianAbsDev*MedianAbsDevScaleFactor(Data.Length)*ControlLimitRejectionCriterion(Data.Length-1) Then ' If value is an outlier
+                If Math.Abs(Data(i)-Median)>MedianAbsDev*MedianAbsDevScaleFactor(Data.Length-1)*ControlLimitRejectionCriterion(Data.Length-1) Then ' If value is an outlier
                     Data(i)=Double.NaN ' Exclude outlier
                 End If
             End If
