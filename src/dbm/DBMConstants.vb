0d4bc9c2efa27522f6179802acccfefda1445125
Johan Fitié <jfitie@gmail.com>
2016-11-25 06:53:35 +0100

    Calculate EMAPreviousPeriods

--- src/dbm/DBMConstants.vb
+++ src/dbm/DBMConstants.vb
@@ -10,7 +10,7 @@ Public Class DBMConstants
 
     Public Const CalculationInterval As Integer=            300 ' seconds; 300 = 5 minutes
     Public Const ComparePatterns As Integer=                12 ' weeks
-    Public Const EMAPreviousPeriods As Integer=             6 ' previous periods; 6 = 35 minutes, current value inclusive
+    Public Const EMAPreviousPeriods As Integer=             CInt(0.5*3600/CalculationInterval) ' previous periods; 6 = 35 minutes, current value inclusive
     Public Const ConfidenceInterval As Double=              0.99 ' confidence interval for outlier detection
     Public Const CorrelationPreviousPeriods As Integer=     CInt(2*3600/CalculationInterval-1) ' previous periods; 23 = 2 hours, current value inclusive
     Public Const CorrelationThreshold As Double=            0.83666 ' absolute correlation lower limit for detecting (anti)correlation
