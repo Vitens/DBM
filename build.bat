808ad17e75570634b349334046ec078ab462c7eb
Johan Fitié <jfitie@gmail.com>
2016-12-04 16:24:39 +0100

    Use wildcards and enable compiler optimizations in build script

--- build.bat
+++ build.bat
@@ -28,29 +28,27 @@ if not exist build mkdir build
 
 del /Q build\*
 
-set vbc="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\Vbc.exe"
-set AlwaysIncludeFiles=src\shared\DBMSharedManifest.vb
-set IncludeFiles=src\dbm\DBM.vb src\dbm\DBMCachedValue.vb src\dbm\DBMConstants.vb src\dbm\DBMCorrelationPoint.vb src\dbm\DBMDataManager.vb src\dbm\DBMFunctions.vb src\dbm\DBMMath.vb src\dbm\DBMPoint.vb src\dbm\DBMResult.vb src\dbm\DBMStatistics.vb
+set vbc="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\Vbc.exe" /optimize+ /nologo
 set PIRefs="%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDK.dll","%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDKCommon.dll"
 set PIACERefs="%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PITimeServer.dll","%PIHOME%\ACE\OSISoft.PIACENet.dll"
 
-%vbc% /target:library /out:build\DBMDriverArray.dll %AlwaysIncludeFiles% %IncludeFiles% src\dbm\driver\DBMDriverArray.vb
+%vbc% /target:library /out:build\DBMDriverArray.dll src\shared\*.vb src\dbm\*.vb src\dbm\driver\DBMDriverArray.vb
 if not exist build\DBMDriverArray.dll goto ExitBuild
 
-%vbc% /reference:build\DBMDriverArray.dll /out:build\DBMUnitTestsArray.exe /define:OfflineUnitTests=True %AlwaysIncludeFiles% test\unit\DBMUnitTests.vb
+%vbc% /reference:build\DBMDriverArray.dll /out:build\DBMUnitTestsArray.exe /define:OfflineUnitTests=True src\shared\*.vb test\unit\DBMUnitTests.vb
 if not exist build\DBMUnitTestsArray.exe goto ExitBuild
 
 if exist "%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDK.dll" (
 
-    %vbc% /reference:%PIRefs% /target:library /out:build\DBMDriverOSIsoftPI.dll %AlwaysIncludeFiles% %IncludeFiles% src\dbm\driver\DBMDriverOSIsoftPI.vb
+    %vbc% /reference:%PIRefs% /target:library /out:build\DBMDriverOSIsoftPI.dll src\shared\*.vb src\dbm\*.vb src\dbm\driver\DBMDriverOSIsoftPI.vb
     if not exist build\DBMDriverOSIsoftPI.dll goto ExitBuild
 
-    %vbc% /reference:%PIRefs%,build\DBMDriverOSIsoftPI.dll /out:build\DBMUnitTestsOSIsoftPI.exe %AlwaysIncludeFiles% test\unit\DBMUnitTests.vb
+    %vbc% /reference:%PIRefs%,build\DBMDriverOSIsoftPI.dll /out:build\DBMUnitTestsOSIsoftPI.exe src\shared\*.vb test\unit\DBMUnitTests.vb
     if not exist build\DBMUnitTestsOSIsoftPI.exe goto ExitBuild
 
     if exist "%PIHOME%\ACE\OSISoft.PIACENet.dll" (
 
-        %vbc% /reference:%PIRefs%,%PIACERefs%,build\DBMDriverOSIsoftPI.dll /rootnamespace:PIACE.DBMRt /target:library /out:build\DBMRt.dll %AlwaysIncludeFiles% src\PIACENet\DBMRt.vb src\PIACENet\DBMRtCalculator.vb src\PIACENet\DBMRtPIPoint.vb src\PIACENet\DBMRtPIServer.vb
+        %vbc% /reference:%PIRefs%,%PIACERefs%,build\DBMDriverOSIsoftPI.dll /rootnamespace:PIACE.DBMRt /target:library /out:build\DBMRt.dll src\shared\*.vb src\PIACENet\*.vb
         if not exist build\DBMRt.dll goto ExitBuild
 
     )
