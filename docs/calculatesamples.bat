15f6b92da9c2b06bf992f684acfa5c66fe90a25a
Johan Fitié <jfitie@gmail.com>
2016-12-28 13:43:09 +0100

    Output sample data in international format

--- docs/calculatesamples.bat
+++ docs/calculatesamples.bat
@@ -24,7 +24,7 @@ rem along with DBM.  If not, see <http://www.gnu.org/licenses/>.
 %~d0
 cd %~dp0
 
-..\build\DBMTester.exe -i=sample1input.csv -st=24-11-2016 -et=29-11-2016 | sort > sample1.csv
-..\build\DBMTester.exe -i=sample2input.csv -st=12-3-2013 -et=13-3-2013 | sort > sample2.csv
-..\build\DBMTester.exe -i=sample3input.csv -c=sample3correlation.csv -st=1-1-2016 -et=2-1-2016 | sort > sample3.csv
-..\build\DBMTester.exe -i=sample4input.csv -c=sample4correlation.csv -st=13-11-2014 -et=14-11-2014 | sort > sample4.csv
+..\build\DBMTester.exe -i=sample1input.csv -st=24-11-2016 -et=29-11-2016 -f=intl | sort > sample1.csv
+..\build\DBMTester.exe -i=sample2input.csv -st=12-3-2013 -et=13-3-2013 -f=intl | sort > sample2.csv
+..\build\DBMTester.exe -i=sample3input.csv -c=sample3correlation.csv -st=1-1-2016 -et=2-1-2016 -f=intl | sort > sample3.csv
+..\build\DBMTester.exe -i=sample4input.csv -c=sample4correlation.csv -st=13-11-2014 -et=14-11-2014 -f=intl | sort > sample4.csv
