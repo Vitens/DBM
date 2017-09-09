d00c08a4eaafe3a8a482d9f95a79d295dc870ee0
Johan Fitié <jfitie@gmail.com>
2017-09-09 19:58:15 +0200

    Add abstract point driver

---
+++ src/dbm/DBMAbstractPointDriver.vb
@@ -0,0 +1,50 @@
+Option Explicit
+Option Strict
+
+
+' DBM
+' Dynamic Bandwidth Monitor
+' Leak detection method implemented in a real-time data historian
+'
+' Copyright (C) 2014, 2015, 2016, 2017  J.H. Fitié, Vitens N.V.
+'
+' This file is part of DBM.
+'
+' DBM is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' DBM is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with DBM.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Namespace Vitens.DynamicBandwidthMonitor
+
+
+  Public MustInherit Class DBMAbstractPointDriver
+
+
+    Public Point As Object
+
+
+    Public Sub New(Point As Object)
+
+      Me.Point = Point
+
+    End Sub
+
+
+    Public MustOverride Function GetData(StartTimestamp As DateTime, _
+      EndTimestamp As DateTime) As Double
+
+
+  End Class
+
+
+End Namespace
