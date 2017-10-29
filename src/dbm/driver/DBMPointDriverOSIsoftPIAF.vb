b6f6f813a0b4dc32cc72528d02839d25f29a7486
Johan Fitié <jfitie@gmail.com>
2017-10-29 20:57:40 +0100

    Initial driver prototype

---
+++ src/dbm/driver/DBMPointDriverOSIsoftPIAF.vb
@@ -0,0 +1,70 @@
+Option Explicit
+Option Strict
+
+
+' DBM
+' Dynamic Bandwidth Monitor
+' Leak detection method implemented in a real-time data historian
+'
+' Copyright (C) 2014, 2015, 2016, 2017  J.H. Fitié, Vitens N.V.
+'
+' This file is part of DBM.
+'
+' DBM is free software: you can redistribute it and/or modify
+' it under the terms of the GNU General Public License as published by
+' the Free Software Foundation, either version 3 of the License, or
+' (at your option) any later version.
+'
+' DBM is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+' GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License
+' along with DBM.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Imports System
+Imports OSIsoft.AF.Data.AFCalculationBasis
+Imports OSIsoft.AF.Data.AFSummaryTypes
+Imports OSIsoft.AF.Data.AFTimestampCalculation
+Imports OSIsoft.AF.PI
+Imports OSIsoft.AF.Time
+
+
+' Assembly title
+<assembly:System.Reflection.AssemblyTitle("DBMPointDriverOSIsoftPIAF")>
+
+
+Namespace Vitens.DynamicBandwidthMonitor
+
+
+  Public Class DBMPointDriver
+    Inherits DBMPointDriverAbstract
+
+
+    ' Description: Driver for OSIsoft PI Asset Framework.
+    ' Identifier (Point): OSIsoft.AF.PI.PIPoint (PI tag)
+
+
+    Public Sub New(Point As Object)
+
+      MyBase.New(Point)
+
+    End Sub
+
+
+    Public Overrides Function GetData(StartTimestamp As DateTime, _
+      EndTimestamp As DateTime) As Double
+
+      Return DirectCast(DirectCast(Point, PIPoint).Summary _
+        (New AFTimeRange(AFTime(StartTimestamp), AFTime(EndTimestamp)), _
+        Average, TimeWeighted, EarliestTime).Item(Average).Value, Double)
+
+    End Function
+
+
+  End Class
+
+
+End Namespace
