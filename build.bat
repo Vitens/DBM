9fe4097617563cefaa4f1f551abff04dc41a773c
Johan Fitié <jfitie@gmail.com>
2017-11-18 11:01:11 +0100

    Remove unused reference

--- build.bat
+++ build.bat
@@ -26,7 +26,6 @@ cd /d %~dp0
 rem Variables
 set vbc="%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\Vbc.exe" /win32icon:res\dbm.ico /optimize+ /nologo /novbruntimeref
 if not defined PIHOME set PIHOME=%CD%\3rdParty\PILibraries
-set PIRefs="%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDK.dll","%PIHOME%\pisdk\PublicAssemblies\OSIsoft.PISDKCommon.dll"
 set PIAFDir=%PIHOME%\AF
 set PIAFRefs="%PIAFDir%\PublicAssemblies\4.0\OSIsoft.AFSDK.dll"
 
