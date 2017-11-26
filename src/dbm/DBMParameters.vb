a35fc3047fe3be66791bf616c42939811d111bf5
Johan Fitié <jfitie@gmail.com>
2017-11-26 09:16:31 +0100

    Automatic optimized cache size (#108)

--- src/dbm/DBMParameters.vb
+++ src/dbm/DBMParameters.vb
@@ -72,11 +72,6 @@ Namespace Vitens.DynamicBandwidthMonitor
     Public Shared RegressionAngleRange As Double = _
       SlopeToAngle(2)-SlopeToAngle(1)
 
-    ' Maximum number of cached prediction results per point.
-    ' Default: Optimized for real-time continuous calculations.
-    Public Shared MaxPointPredictions As Integer = _
-      EMAPreviousPeriods+2*CorrelationPreviousPeriods+1
-
 
   End Class
 
