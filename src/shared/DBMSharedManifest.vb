d963e8d564d3bb2157e6d5cbde17df82cbfc60ca
Johan Fitié <jfitie@gmail.com>
2017-11-30 13:33:43 +0100

    Remove version comment

--- src/shared/DBMSharedManifest.vb
+++ src/shared/DBMSharedManifest.vb
@@ -28,7 +28,7 @@ Option Strict
 ' returned using the DBM.Version function.
 
 
-<assembly:System.Reflection.AssemblyVersion("1.12.10.*")> ' Major.Minor.Rev.*
+<assembly:System.Reflection.AssemblyVersion("1.12.10.*")>
 ' Note: when updating the version number, also modify version in appveyor.yml
 '       for AppVeyor continuous integration to use the same version numbering.
 
