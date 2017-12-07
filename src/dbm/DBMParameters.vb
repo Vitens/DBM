15711bd9ec972fe9f380f43c7ada8fcab5007cb9
Johan Fitié <jfitie@gmail.com>
2017-12-07 21:28:52 +0100

    Optimize parameters

--- src/dbm/DBMParameters.vb
+++ src/dbm/DBMParameters.vb
@@ -37,7 +37,10 @@ Namespace Vitens.DynamicBandwidthMonitor
     ' This class contains default values for several parameters which DBM uses
     ' for its calculations. The values for these parameters can be changed
     ' at runtime.
-
+    ' The default parameter values where determined by analysing the results
+    ' of a particle swarm optimization method using real production data from
+    ' Vitens, and then manually tweaking these for optimal results and
+    ' performance.
 
     ' Time interval at which the calculation is run (seconds).
     ' Default: 5 minutes.
@@ -45,32 +48,37 @@ Namespace Vitens.DynamicBandwidthMonitor
 
     ' Number of weeks to look back to predict the current value
     ' and control limits.
-    Public Shared ComparePatterns As Integer = 12
+    ' Default: 18 weeks (Avg:20.8 SD:2.278 Delta:-13.3%)
+    Public Shared ComparePatterns As Integer = 18
 
     ' Number of previous intervals used to smooth the data.
-    ' Default: 35 minutes, current value inclusive.
+    ' Default: 11 (Avg:22.5 SD:1.658 Delta:-51.1%), one hour (reduced to
+    '  reduce lag)
     Public Shared EMAPreviousPeriods As Integer = _
-      CInt(0.5*3600/CalculationInterval)
+      CInt(1*3600/CalculationInterval-1)
 
     ' Confidence interval used for removing outliers and determining
     ' control limits (ratio).
-    Public Shared ConfidenceInterval As Double = 0.99
+    ' Default: 0.93 (Avg:0.92834 SD:0.003 Delta:0.2%)
+    Public Shared ConfidenceInterval As Double = 0.93
 
     ' Number of previous intervals used to calculate prediction error
     ' correlation when an exception is found.
-    ' Default: 2 hours, current value inclusive.
+    ' Default: 53 (Avg:55.3 SD:1.785 Delta:-4.1%), 4.5 hours
     Public Shared CorrelationPreviousPeriods As Integer = _
-      CInt(2*3600/CalculationInterval-1)
+      CInt(4.5*3600/CalculationInterval-1)
 
     ' Absolute correlation lower limit for detecting (anti)correlation.
-    ' Default: 0.83666 for a determination of 0.7.
-    Public Shared CorrelationThreshold As Double = Sqrt(0.7)
+    ' Default: 0.77460 (Avg:0.77990 SD:0.017 Delta:-0.7%) for a
+    '  determination of 0.6
+    Public Shared CorrelationThreshold As Double = Sqrt(0.6)
 
     ' Regression angle range (around -45/+45 degrees) required when suppressing
     ' based on (anti)correlation (degrees).
-    ' Default: Allow factor 2 difference between values (18.435 degrees).
+    ' Default: 21.03751 (Avg:20.77809 SD:1.368 Delta:1.2%), allow factor 2.25
+    '  difference between values
     Public Shared RegressionAngleRange As Double = _
-      SlopeToAngle(2)-SlopeToAngle(1)
+      SlopeToAngle(2.25)-SlopeToAngle(1)
 
 
   End Class
