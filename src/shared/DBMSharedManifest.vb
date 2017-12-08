48d034346c1f2ef60ab9b8cad9bf906327400f9e
Johan Fitié <jfitie@gmail.com>
2017-12-08 09:05:30 +0100

    Update version

--- src/shared/DBMSharedManifest.vb
+++ src/shared/DBMSharedManifest.vb
@@ -28,7 +28,7 @@ Option Strict
 ' returned using the DBM.Version function.
 
 
-<assembly:System.Reflection.AssemblyVersion("1.12.10.*")>
+<assembly:System.Reflection.AssemblyVersion("1.12.11.*")>
 ' Note: when updating the version number, also modify version in appveyor.yml
 '       for AppVeyor continuous integration to use the same version numbering.
 
