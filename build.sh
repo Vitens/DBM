16119fff8233609d0518933b661fe35d08d3efb0
Johan Fitié <jfitie@gmail.com>
2017-12-10 20:50:59 +0100

    Remove cd command from Linux build script

--- build.sh
+++ build.sh
@@ -21,8 +21,6 @@
 # You should have received a copy of the GNU General Public License
 # along with DBM.  If not, see <http://www.gnu.org/licenses/>.
 
-cd "`dirname "$0"`"
-
 export WINEDEBUG=fixme-all,err-ole
 export WINEDLLOVERRIDES=mscoree=n
 wine cmd /c build.bat
