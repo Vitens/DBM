28825c668407761c60ec4ed77ce327087c89cb75
Johan Fitié <jfitie@gmail.com>
2017-12-14 18:53:19 +0100

    Improve parameter comments

--- src/dbm/DBMParameters.vb
+++ src/dbm/DBMParameters.vb
@@ -43,40 +43,44 @@ Namespace Vitens.DynamicBandwidthMonitor
     ' performance.
 
 
-    ' Time interval at which the calculation is run (seconds).
-    ' Default: 5 minutes.
+    ' Time interval at which the calculation is run.
+    '  Default: 300 seconds, 5 minutes.
     Public Shared CalculationInterval As Integer = 300
 
-    ' Number of weeks to look back to predict the current value
-    ' and control limits.
-    ' Default: 17 weeks (PSO Avg:20.8 SD:2.278 Delta:-18.3%), 4 months
+    ' Number of weeks to look back to predict the current value and
+    ' control limits.
+    '  Default: 17 weeks, 4 months.
+    '  PSO; Avg: 20.8, SD: 2.278, Delta: -18.3%
     Public Shared ComparePatterns As Integer = CInt(52/12*4)
 
     ' Number of previous intervals used to smooth the data.
-    ' Default: 5 intervals (PSO Avg:22.5 SD:1.658 Delta:-77.8%), 30 minutes
-    '  (reduced to minimize lag)
+    '  Default: 5 intervals, 30 minutes.
+    '  PSO; Avg: 22.5, SD: 1.658, Delta: -77.8% (to minimize lag)
     Public Shared EMAPreviousPeriods As Integer = _
       CInt(0.5*3600/CalculationInterval-1)
 
     ' Confidence interval used for removing outliers and determining
-    ' control limits (ratio).
-    ' Default: 0.99 (PSO Avg:0.92834 SD:0.003 Delta:+6.6%)
+    ' control limits.
+    '  Default: 0.99.
+    '  PSO; Avg: 0.92834, SD: 0.003, Delta: +6.6% (to reduce false positives)
     Public Shared ConfidenceInterval As Double = 0.99
 
     ' Number of previous intervals used to calculate prediction error
     ' correlation when an exception is found.
-    ' Default: 119 intervals (PSO Avg:133.6 SD:1.356 Delta:-10.9%), 10 hours
+    '  Default: 119 intervals, 10 hours.
+    '  PSO; Avg: 133.6, SD: 1.356, Delta: -10.9%
     Public Shared CorrelationPreviousPeriods As Integer = _
       CInt(10*3600/CalculationInterval-1)
 
     ' Absolute correlation lower limit for detecting (anti)correlation.
-    ' Default: 0.7 (PSO Avg:0.70116 SD:0.002 Delta:-0.2%)
+    '  Default: 0.7.
+    '  PSO; Avg: 0.70116, SD: 0.002, Delta: -0.2%
     Public Shared CorrelationThreshold As Double = 0.7
 
     ' Regression angle range (around -45/+45 degrees) required when suppressing
-    ' based on (anti)correlation (degrees).
-    ' Default: 26.56505 degrees (PSO Avg:26.94275 SD:3.473 Delta:-1.4%), allow
-    '  factor 3 difference between values
+    ' based on (anti)correlation.
+    '  Default: 26.56505 degrees, allow factor 3 difference between values.
+    '  PSO; Avg: 26.94275, SD: 3.473, Delta: -1.4%
     Public Shared RegressionAngleRange As Double = _
       SlopeToAngle(3)-SlopeToAngle(1)
 
