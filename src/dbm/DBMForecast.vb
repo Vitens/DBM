cc5687a9a41411b0cb6bd7a23df8763ca397770a
Johan Fitié <jfitie@gmail.com>
2018-04-19 18:50:14 +0200

    Improve Range function results

--- src/dbm/DBMForecast.vb
+++ src/dbm/DBMForecast.vb
@@ -46,12 +46,14 @@ Namespace Vitens.DynamicBandwidthMonitor
         ' Returns the range between the control limits and forecast value scaled
         ' for the requested confidence interval. This function requires that the
         ' results have been calculated for the default range using the Forecast
-        ' function before. Always use the standard normal distribution here as
-        ' we perform this calculation on an EMA-smoothed series of data and not
-        ' on a single forecast result.
+        ' function before. Always use the full sample size and appropriate
+        ' distribution as the calculation is run on an EMA-smoothed series of
+        ' data and not on a single forecast result from which outliers might be
+        ' removed.
 
         Return (UpperControlLimit-ForecastValue)/
-          NormSInv(BandwidthCI)*NormSInv(p)
+          ControlLimitRejectionCriterion(2*BandwidthCI-1, ComparePatterns-1)*
+          ControlLimitRejectionCriterion(2*p-1, ComparePatterns-1)
 
       End Function
 
