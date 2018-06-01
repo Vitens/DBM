84c6d833254fe40d753b6453185371dbf7368c88
Johan Fitié <jfitie@gmail.com>
2018-06-01 08:22:33 +0200

    Improve performance by fixing caching issue (#212)
    * Initial commit
    
    * Simplify

--- src/dbm/DBM.vb
+++ src/dbm/DBM.vb
@@ -176,10 +176,12 @@ Namespace Vitens.DynamicBandwidthMonitor
         -CalculationInterval).AddDays(ComparePatterns*-7)
       EndTimestamp = AlignTimestamp(EndTimestamp, CalculationInterval)
 
-      InputPointDriver.PrepareData(StartTimestamp, EndTimestamp)
+      Point(InputPointDriver).PointDriver.PrepareData(
+        StartTimestamp, EndTimestamp)
       If CorrelationPoints IsNot Nothing Then
         For Each CorrelationPoint In CorrelationPoints
-          CorrelationPoint.PointDriver.PrepareData(StartTimestamp, EndTimestamp)
+          Point(CorrelationPoint.PointDriver).PointDriver.PrepareData(
+            StartTimestamp, EndTimestamp)
         Next
       End If
 
