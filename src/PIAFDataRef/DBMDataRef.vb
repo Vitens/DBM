776843c4f12758e40bfc7d760e6ab55467a357a1
Johan Fitié <jfitie@gmail.com>
2019-10-30 09:47:35 +0100

    @jfitie [DR] Add SyncLock timeout to GetValues method

--- src/PIAFDataRef/DBMDataRef.vb
+++ src/PIAFDataRef/DBMDataRef.vb
@@ -26,6 +26,7 @@ Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.DateTime
+Imports System.Double
 Imports System.Math
 Imports System.Runtime.InteropServices
 Imports System.Threading
@@ -200,56 +201,58 @@ Namespace Vitens.DynamicBandwidthMonitor
 
       ' Return DBM result parameter based on applied property/trait.
 
-      Dim AlignedTimestamp As AFTime ' DST issue with DateTime obj in DBMResult
-      Dim Value As New AFValue
+      Dim AlignedTimestamp As AFTime = New AFTime(Timestamp.UtcSeconds-
+        Timestamp.UtcSeconds Mod CalculationInterval) ' Align previous interval
+      Dim Value As AFValue = New AFValue(NaN, AlignedTimestamp) ' Default to NaN
 
-      Monitor.Enter(DBM) ' Request the lock, and block until it is obtained.
-      Try
+      ' Request the lock, and block until it is obtained. If this takes longer
+      ' than one calculation interval, return Not a Number.
+      If Monitor.TryEnter(DBM, TimeSpan.FromSeconds(CalculationInterval)) Then
+        Try
 
-        If PointsStale.IsStale Then UpdatePoints ' Update points periodically
-        If Not EndTimestamp.IsEmpty Then DBM.PrepareData(InputPointDriver,
-          CorrelationPoints, Timestamp.LocalTime, EndTimestamp.LocalTime)
+          If PointsStale.IsStale Then UpdatePoints ' Update points periodically
+          If Not EndTimestamp.IsEmpty Then DBM.PrepareData(InputPointDriver,
+            CorrelationPoints, Timestamp.LocalTime, EndTimestamp.LocalTime)
 
-        With DBM.Result(
-          InputPointDriver, CorrelationPoints, Timestamp.LocalTime)
+          With DBM.Result(
+            InputPointDriver, CorrelationPoints, Timestamp.LocalTime)
 
-          AlignedTimestamp = New AFTime(Timestamp.UtcSeconds-
-            Timestamp.UtcSeconds Mod CalculationInterval) ' Align prev interval
-          If Attribute.Trait Is Nothing Then
-            Value = New AFValue(.Factor, AlignedTimestamp)
-            Value.Questionable = .HasEvent
-            Value.Substituted = .HasSuppressedEvent
-          ElseIf Attribute.Trait Is LimitTarget Then
-            Value = New AFValue(.ForecastItem.Measurement, AlignedTimestamp)
-          ElseIf Attribute.Trait Is Forecast Then
-            Value = New AFValue(.ForecastItem.ForecastValue, AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitMinimum Then
-            Value = New AFValue(.ForecastItem.ForecastValue-
-              .ForecastItem.Range(pValueMinMax), AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitLoLo Then
-            Value = New AFValue(.ForecastItem.LowerControlLimit,
-              AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitLo Then
-            Value = New AFValue(.ForecastItem.ForecastValue-
-              .ForecastItem.Range(pValueLoHi), AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitHi Then
-            Value = New AFValue(.ForecastItem.ForecastValue+
-              .ForecastItem.Range(pValueLoHi), AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitHiHi Then
-            Value = New AFValue(.ForecastItem.UpperControlLimit,
-              AlignedTimestamp)
-          ElseIf Attribute.Trait Is LimitMaximum Then
-            Value = New AFValue(.ForecastItem.ForecastValue+
-              .ForecastItem.Range(pValueMinMax), AlignedTimestamp)
-          End If
+            If Attribute.Trait Is LimitTarget Then
+              Value = New AFValue(.ForecastItem.Measurement, AlignedTimestamp)
+            ElseIf Attribute.Trait Is Forecast Then
+              Value = New AFValue(.ForecastItem.ForecastValue, AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitMinimum Then
+              Value = New AFValue(.ForecastItem.ForecastValue-
+                .ForecastItem.Range(pValueMinMax), AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitLoLo Then
+              Value = New AFValue(.ForecastItem.LowerControlLimit,
+                AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitLo Then
+              Value = New AFValue(.ForecastItem.ForecastValue-
+                .ForecastItem.Range(pValueLoHi), AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitHi Then
+              Value = New AFValue(.ForecastItem.ForecastValue+
+                .ForecastItem.Range(pValueLoHi), AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitHiHi Then
+              Value = New AFValue(.ForecastItem.UpperControlLimit,
+                AlignedTimestamp)
+            ElseIf Attribute.Trait Is LimitMaximum Then
+              Value = New AFValue(.ForecastItem.ForecastValue+
+                .ForecastItem.Range(pValueMinMax), AlignedTimestamp)
+            Else
+              Value = New AFValue(.Factor, AlignedTimestamp)
+              Value.Questionable = .HasEvent
+              Value.Substituted = .HasSuppressedEvent
+            End If
 
-        End With
+          End With
 
-        Return Value
+        Finally
+          Monitor.Exit(DBM) ' Ensure that the lock is released.
+        End Try
+      End If
 
-      Finally
-        Monitor.Exit(DBM) ' Ensure that the lock is released.
-      End Try
+      Return Value
 
     End Function
 
@@ -278,30 +281,31 @@ Namespace Vitens.DynamicBandwidthMonitor
       ' Returns values for each interval in a time range. The (aligned) end time
       ' itself is excluded.
 
-      Dim IntervalSeconds As Double
+      Dim IntervalSeconds As Double = Max(1, ((timeContext.EndTime.UtcSeconds-
+        timeContext.StartTime.UtcSeconds)/CalculationInterval-1)/
+        (numberOfValues-1))*CalculationInterval ' Required interval
 
       GetValues = New AFValues
-      IntervalSeconds = Max(1, ((timeContext.EndTime.UtcSeconds-timeContext.
-        StartTime.UtcSeconds)/CalculationInterval-1)/(numberOfValues-1))*
-        CalculationInterval ' Required interval, first and last interv inclusive
 
       ' SyncLock here so that when multiple parallel threads call the GetValues
       ' method, the DBMResult method only executes in one thread at a time for
       ' all timestamps sequentially. This ensures that the cache is used
       ' optimally and prevents reloading data for each thread after each call to
-      ' GetValue.
-      Monitor.Enter(DBM) ' Request the lock, and block until it is obtained.
-      Try
+      ' GetValue. Request the lock, and block until it is obtained. If this
+      ' takes longer than one calculation interval, return no values.
+      If Monitor.TryEnter(DBM, TimeSpan.FromSeconds(CalculationInterval)) Then
+        Try
 
-        Do While timeContext.EndTime > timeContext.StartTime
-          GetValues.Add(DBMResult(timeContext.StartTime, timeContext.EndTime))
-          timeContext.StartTime = New AFTime(
-            timeContext.StartTime.UtcSeconds+IntervalSeconds)
-        Loop
+          Do While timeContext.EndTime > timeContext.StartTime
+            GetValues.Add(DBMResult(timeContext.StartTime, timeContext.EndTime))
+            timeContext.StartTime = New AFTime(
+              timeContext.StartTime.UtcSeconds+IntervalSeconds)
+          Loop
 
-      Finally
-        Monitor.Exit(DBM) ' Ensure that the lock is released.
-      End Try
+        Finally
+          Monitor.Exit(DBM) ' Ensure that the lock is released.
+        End Try
+      End If
 
       Return GetValues
 
