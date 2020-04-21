ff190f33076a3ea5f03e43c5a72ac0316a95e431
Johan Fitié <jfitie@gmail.com>
2020-04-21 08:18:09 +0200

    [DR] SyncLock stale points check and update

--- src/PIAFDataRef/DBMDataRef.vb
+++ src/PIAFDataRef/DBMDataRef.vb
@@ -237,7 +237,15 @@ Namespace Vitens.DynamicBandwidthMonitor
           TimeSpan.FromSeconds(Sqrt(CalculationInterval)/2)) Then
           Try
 
-            If PointsStale.IsStale Then UpdatePoints ' Update points if stale
+            Monitor.Enter(PointsStale)
+            Try
+              If PointsStale.IsStale Then
+                UpdatePoints ' Update points if stale
+              End If
+            Finally
+              Monitor.Exit(PointsStale)
+            End Try
+
             If timeContext.EndTime.UtcSeconds-timeContext.StartTime.
               UtcSeconds >= 2*CalculationInterval Then DBM.PrepareData(
               InputPointDriver, CorrelationPoints,
