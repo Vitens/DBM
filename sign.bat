144a771666ff294bc86dc28727a912a7aeae522e
Johan Fitié <jfitie@gmail.com>
2020-05-21 09:52:29 +0000

    Simplify

--- sign.bat
+++ sign.bat
@@ -23,4 +23,4 @@ cd /d %~dp0
 
 rem Sign
 powershell -Command "Set-AuthenticodeSignature -Certificate (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('%2', '%3')) -TimestampServer http://timestamp.digicert.com -HashAlgorithm SHA256 -FilePath '%1'" > NUL
-powershell -Command "$c = Get-AuthenticodeSignature -FilePath '%1'; (Split-Path '%1' -Leaf) + ' signed by ' + $c.SignerCertificate.Subject"
+powershell -Command "' 🔑 ' + (Split-Path '%1' -Leaf) + ' signed by ' + (Get-AuthenticodeSignature -FilePath '%1').SignerCertificate.Subject"
