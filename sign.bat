25a20fde2500617979c26747f4b4b006bf683b79
Johan Fitié <jfitie@gmail.com>
2020-05-25 08:17:16 +0000

    Simplify sign script

--- sign.bat
+++ sign.bat
@@ -22,6 +22,5 @@ rem along with DBM.  If not, see <http://www.gnu.org/licenses/>.
 cd /d %~dp0
 
 rem Sign
-powershell -Command "Try {Set-AuthenticodeSignature -Certificate (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('%2', '%3')) -TimestampServer http://timestamp.digicert.com -HashAlgorithm SHA256 -FilePath '%1' | Out-Null} Catch {Write-Host $_; Exit 1}"
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
-powershell -Command "Write-Host ((Split-Path '%1' -Leaf) + ' signed by ' + (Get-AuthenticodeSignature -FilePath '%1').SignerCertificate.Subject)"
+powershell -Command "Try {Set-AuthenticodeSignature -Certificate (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('%2', '%3')) -TimestampServer http://timestamp.digicert.com -HashAlgorithm SHA256 -FilePath '%1' | Out-Null; Write-Host ((Split-Path '%1' -Leaf) + ' signed by ' + (Get-AuthenticodeSignature -FilePath '%1').SignerCertificate.Subject)} Catch {Write-Host $_; Exit 1}"
+exit %ERRORLEVEL%
