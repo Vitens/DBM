4fe986a0e8d7fd22a48d3172e643bc5e83e59fed
Johan Fitié <jfitie@gmail.com>
2020-05-29 12:16:02 +0200

    Solve .Net modulo issue

--- src/dbm/DBMMath.vb
+++ src/dbm/DBMMath.vb
@@ -413,13 +413,9 @@ Namespace Vitens.DynamicBandwidthMonitor
 
       If Interval = 0 Then Return NaN
 
-      If Value > 0 And Interval < 0 Then
+      If Sign(Value) <> Sign(Interval) Then ' Solve .Net modulo issue
         Value = Value - Interval
         Interval = Abs(Interval)
-      End If
-
-      If Value < 0 And Interval > 0 Then
-        Value = Value - Interval
       End If
 
       Return (Value-Value Mod Interval)
