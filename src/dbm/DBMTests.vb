860b914bdab3d589d84a0aa8f7f679506376c3f9
Johan Fitié <jfitie@gmail.com>
2020-06-05 10:33:48 +0000

    Revert "CI: Force test fail"
    This reverts commit 86620420b0789d536de0bedf4d301cfeade88b2c.

--- src/dbm/DBMTests.vb
+++ src/dbm/DBMTests.vb
@@ -1146,7 +1146,7 @@ Namespace Vitens.DynamicBandwidthMonitor
       AssertTrue(PIAFShouldPrepareData(1201))
       AssertTrue(PIAFShouldPrepareData(1499))
       AssertTrue(PIAFShouldPrepareData(1500))
-      AssertFalse(PIAFShouldPrepareData(1501))
+      AssertTrue(PIAFShouldPrepareData(1501))
 
     End Sub
 
