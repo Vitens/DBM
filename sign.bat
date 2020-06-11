d1b235e54f6df5849489d5cdd05d5e82ff70be02
Johan Fitié <jfitie@gmail.com>
2020-06-11 07:13:46 +0200

    Code signing scripts and documentation (#247)
    * Remove pfx
    
    * Do not register on AF server
    
    * Add message before registering on AF server
    
    * Add Sign All tool
    
    * Be quieter
    
    * Quiet signing, trap error
    
    * Documentation
    
    * Only one signing script
    
    * Comment

--- sign.bat
+++ sign.bat
@@ -22,5 +22,5 @@ rem along with DBM.  If not, see <http://www.gnu.org/licenses/>.
 cd /d %~dp0
 
 rem Sign
-powershell -Command "try { Set-AuthenticodeSignature -Certificate (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('%2', '%3')) -TimestampServer http://timestamp.digicert.com -HashAlgorithm SHA256 -FilePath '%1' | Out-Null } catch { Write-Host $_; exit 1 }"
-if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
+rem arguments: pfx file, password
+for %%f in (build\*.dll build\*.exe) do powershell -Command "Set-AuthenticodeSignature -Certificate (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('%1', '%2')) -TimestampServer http://timestamp.digicert.com -HashAlgorithm SHA256 -FilePath '%%f'"
