22e36d12d116156a92476f8604f6d2b4b6175d6d
Johan Fitié <jfitie@gmail.com>
2020-07-01 05:42:42 +0000

    Remove threading from EventSource

--- src/PIAFDataRef/DBMEventSource.vb
+++ src/PIAFDataRef/DBMEventSource.vb
@@ -25,7 +25,6 @@ Option Strict
 Imports System
 Imports System.Collections.Generic
 Imports System.DateTime
-Imports System.Threading
 Imports OSIsoft.AF.Asset
 Imports OSIsoft.AF.Data
 Imports OSIsoft.AF.Time
@@ -40,60 +39,7 @@ Namespace Vitens.DynamicBandwidthMonitor
     Inherits AFEventSource
 
 
-    Private Shared DataPipeEvents As New List(Of DataPipeEvent)
     Private LastEvent As AFTime = Now
-
-
-    Private Structure DataPipeEvent
-
-      ' This structure is used to store events retrieved in a separate thread
-      ' per attribute.
-
-      Public Attribute As AFAttribute
-      Public Value As AFValue
-
-    End Structure
-
-
-    Private Structure RetrievalInfo
-
-      ' This structure is used to pass information required to retrieve events
-      ' to the thread started for each attribute.
-
-      Public StartTime As AFTime
-      Public EndTime As AFTime
-      Public Attribute As AFAttribute
-
-    End Structure
-
-
-    Private Shared Sub RetrieveEvents(RetrievalInfo As Object)
-
-      Dim Values As AFValues
-      Dim DataPipeEvent As DataPipeEvent
-      Dim Value As AFValue
-
-      ' Call the GetValues method for the attribute to retrieve events to store
-      ' in the shared events list.
-      Values = DirectCast(RetrievalInfo, RetrievalInfo).Attribute.
-        GetValues(New AFTimeRange(
-        DirectCast(RetrievalInfo, RetrievalInfo).StartTime,
-        DirectCast(RetrievalInfo, RetrievalInfo).EndTime), 0, Nothing)
-
-      ' Store events in shared events list.
-      Monitor.Enter(DataPipeEvents)
-      Try
-        DataPipeEvent.Attribute = DirectCast(
-          RetrievalInfo, RetrievalInfo).Attribute
-        For Each Value In Values
-          DataPipeEvent.Value = Value
-          DataPipeEvents.Add(DataPipeEvent)
-        Next
-      Finally
-        Monitor.Exit(DataPipeEvents)
-      End Try
-
-    End Sub
 
 
     Protected Overrides Function GetEvents As Boolean
@@ -102,11 +48,8 @@ Namespace Vitens.DynamicBandwidthMonitor
       ' of record.
 
       Dim CurrentInterval As AFTime = Now
-      Dim RetrievalInfo As RetrievalInfo
       Dim Attribute As AFAttribute
-      Dim Threads As New List(Of Thread)
-      Dim Thread As Thread
-      Dim DataPipeEvent As DataPipeEvent
+      Dim Value As AFValue
 
       ' Current time aligned to the previous calculation interval.
       CurrentInterval = New AFTime(AlignPreviousInterval(
@@ -115,37 +58,23 @@ Namespace Vitens.DynamicBandwidthMonitor
       ' Only check for new events once per calculation interval.
       If LastEvent < CurrentInterval Then
 
-        RetrievalInfo.StartTime = LastEvent
-        RetrievalInfo.EndTime = CurrentInterval
-
-        LastEvent = CurrentInterval
-
         For Each Attribute In MyBase.Signups
 
           If Attribute IsNot Nothing Then
 
-            RetrievalInfo.Attribute = Attribute
-            ' Start a new thread for each attribute, passing information about
-            ' the attribute and the time range to retrieve events for.
-            Threads.Add(New Thread(
-              New ParameterizedThreadStart(AddressOf RetrieveEvents)))
-            Threads(Threads.Count-1).Start(RetrievalInfo)
+            For Each Value In Attribute.GetValues(
+              New AFTimeRange(LastEvent, CurrentInterval), 0, Nothing)
+
+              MyBase.PublishEvent(Attribute,
+                New AFDataPipeEvent(AFDataPipeAction.Add, Value))
+
+            Next
 
           End If
 
         Next
 
-        ' Wait for all threads to finish.
-        For Each Thread In Threads
-          Thread.Join
-        Next
-
-        ' Publish all events to the data pipe.
-        For Each DataPipeEvent In DataPipeEvents
-          MyBase.PublishEvent(DataPipeEvent.Attribute,
-            New AFDataPipeEvent(AFDataPipeAction.Add, DataPipeEvent.Value))
-        Next
-        DataPipeEvents.Clear
+        LastEvent = CurrentInterval
 
       End If
 
