429cbd4a88dd50b9439630e60a5d1bac3f5b021a
Johan Fitié <jfitie@gmail.com>
2020-07-05 08:45:22 +0200

    ES Rename variables

--- src/PIAFDataRef/DBMEventSource.vb
+++ src/PIAFDataRef/DBMEventSource.vb
@@ -39,7 +39,7 @@ Namespace Vitens.DynamicBandwidthMonitor
     Inherits AFEventSource
 
 
-    Private LastEvents As New Dictionary(Of AFAttribute, AFTime)
+    Private PreviousEndTimestamps As New Dictionary(Of AFAttribute, AFTime)
 
 
     Protected Overrides Function GetEvents As Boolean
@@ -48,29 +48,29 @@ Namespace Vitens.DynamicBandwidthMonitor
       ' of record.
 
       Dim Attribute As AFAttribute
-      Dim InputSnapshot As AFTime
+      Dim EndTimestamp As AFTime
       Dim Value As AFValue
 
       For Each Attribute In MyBase.Signups
 
         ' Attribute snapshot time aligned to the next calculation interval.
-        InputSnapshot = New AFTime(AlignNextInterval(Attribute.GetValue.
+        EndTimestamp = New AFTime(AlignNextInterval(Attribute.GetValue.
           Timestamp.UtcSeconds, CalculationInterval))
 
-        If Not LastEvents.ContainsKey(Attribute) Then
-          LastEvents.Add(Attribute, InputSnapshot)
+        If Not PreviousEndTimestamps.ContainsKey(Attribute) Then
+          PreviousEndTimestamps.Add(Attribute, EndTimestamp)
         End If
 
         ' Only check for new events once per calculation interval.
-        If LastEvents.Item(Attribute) < InputSnapshot Then
+        If PreviousEndTimestamps.Item(Attribute) < EndTimestamp Then
 
           For Each Value In Attribute.GetValues(New AFTimeRange(
-            LastEvents.Item(Attribute), InputSnapshot), 0, Nothing)
+            PreviousEndTimestamps.Item(Attribute), EndTimestamp), 0, Nothing)
             MyBase.PublishEvent(Attribute,
               New AFDataPipeEvent(AFDataPipeAction.Add, Value))
           Next
 
-          LastEvents.Item(Attribute) = InputSnapshot
+          PreviousEndTimestamps.Item(Attribute) = EndTimestamp
 
         End If
 
