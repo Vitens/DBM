c6fbd3b3b77c235c9ba34f8783700916c0a681cf
Johan Fitié <jfitie@gmail.com>
2020-07-27 13:19:50 +0000

    Tester: test single timestamp

--- samples/runsamples.bat
+++ samples/runsamples.bat
@@ -21,7 +21,7 @@ rem along with DBM.  If not, see <http://www.gnu.org/licenses/>.
 
 cd /d %~dp0
 
-..\build\DBMTester.exe -f=intl -i=sample1input.csv -st=11-24-2016 -et=11-29-2016 > sample1.csv
+..\build\DBMTester.exe -f=intl -i=sample1input.csv -st=11-24-2016 > sample1.csv
 ..\build\DBMTester.exe -f=intl -i=sample2input.csv -st=03-12-2013 -et=03-13-2013 > sample2.csv
 ..\build\DBMTester.exe -f=intl -us=False -i=sample3input.csv -c=sample3correlation.csv -st=01-01-2016 -et=01-02-2016 > sample3.csv
 ..\build\DBMTester.exe -f=intl -i=sample4input.csv -c=sample4correlation.csv -st=11-13-2014 -et=11-14-2014 > sample4.csv
