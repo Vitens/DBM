eeed2e446d3c5794652874acd3c38e8d7a8a3428
Johan Fitié <jfitie@gmail.com>
2020-07-27 10:59:14 +0000

    CI PS Hashes

--- samples/runsamples.bat
+++ samples/runsamples.bat
@@ -25,9 +25,3 @@ cd /d %~dp0
 ..\build\DBMTester.exe -f=intl -i=sample2input.csv -st=03-12-2013 -et=03-13-2013 > sample2.csv
 ..\build\DBMTester.exe -f=intl -us=False -i=sample3input.csv -c=sample3correlation.csv -st=01-01-2016 -et=01-02-2016 > sample3.csv
 ..\build\DBMTester.exe -f=intl -i=sample4input.csv -c=sample4correlation.csv -st=11-13-2014 -et=11-14-2014 > sample4.csv
-
-rem Expected MD5 hash of output files:
-rem   6cb141b48632e8ecabea8320886a6983  sample1.csv
-rem   fc7b57bd245d877d1bb9c3098aba75de  sample2.csv
-rem   32f98957d92a37c4d588535910cf003a  sample3.csv
-rem   840bc8aa797b961c8764b43b788dbb03  sample4.csv
