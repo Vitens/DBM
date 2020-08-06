202e5e8b87e72ab0804a2fbc5d9fa209a185d02b
Johan Fitié <jfitie@gmail.com>
2020-08-06 05:22:07 +0000

    Simplify EventSource

--- src/PIAFDataRef/DBMEventSource.vb
+++ src/PIAFDataRef/DBMEventSource.vb
@@ -24,12 +24,8 @@ Option Strict
 
 Imports System
 Imports System.Collections.Generic
-Imports System.DateTime
-Imports System.Threading
 Imports OSIsoft.AF.Asset
 Imports OSIsoft.AF.Data
-Imports OSIsoft.AF.Time
-Imports Vitens.DynamicBandwidthMonitor.DBMDate
 
 
 Namespace Vitens.DynamicBandwidthMonitor
@@ -39,62 +35,7 @@ Namespace Vitens.DynamicBandwidthMonitor
     Inherits AFEventSource
 
 
-    Private Shared PreviousSnapshots As New Dictionary(Of AFAttribute, AFTime)
-    Private Shared DataPipeEvents As New List(Of DataPipeEvent)
-    Private PreviousTimestamp As DateTime
-
-
-    Private Structure DataPipeEvent
-
-      ' This structure is used to store events retrieved in a separate thread
-      ' per attribute.
-
-      Public Attribute As AFAttribute
-      Public Value As AFValue
-
-    End Structure
-
-
-    Private Shared Sub RetrieveEvent(Attribute As Object)
-
-      Dim DataPipeEvent As DataPipeEvent
-      Dim Snapshot As AFValue
-
-      ' In a new interval, retrieve snapshot value.
-      DataPipeEvent.Attribute = DirectCast(Attribute, AFAttribute)
-      Snapshot = DataPipeEvent.Attribute.GetValue
-
-      Monitor.Enter(PreviousSnapshots) ' Lock
-      Try
-        If Not PreviousSnapshots.ContainsKey(DataPipeEvent.Attribute) Then
-          ' Store the initial previous snapshot timestamp for this attribute.
-          PreviousSnapshots.Add(DataPipeEvent.Attribute, Snapshot.Timestamp)
-        Else
-          ' If newer, store snapshot timestamp as previous snapshot timestamp
-          ' for this attribute.
-          If Snapshot.Timestamp >
-            PreviousSnapshots.Item(DataPipeEvent.Attribute) Then
-            PreviousSnapshots.Item(DataPipeEvent.Attribute) = Snapshot.Timestamp
-          Else
-            Snapshot = Nothing ' There is no new event.
-          End If
-        End If
-      Finally
-        Monitor.Exit(PreviousSnapshots)
-      End Try
-
-      If Snapshot IsNot Nothing Then
-        ' Store new snapshot in shared events list.
-        DataPipeEvent.Value = Snapshot
-        Monitor.Enter(DataPipeEvents) ' Lock
-        Try
-          DataPipeEvents.Add(DataPipeEvent)
-        Finally
-          Monitor.Exit(DataPipeEvents)
-        End Try
-      End If
-
-    End Sub
+    Private PreviousSnapshots As New Dictionary(Of AFAttribute, DateTime)
 
 
     Protected Overrides Function GetEvents As Boolean
@@ -102,62 +43,41 @@ Namespace Vitens.DynamicBandwidthMonitor
       ' The GetEvents method is designed to get data pipe events from the System
       ' of record.
 
-      Dim CurrentTimestamp As DateTime
       Dim Attribute As AFAttribute
-      Dim Threads As New List(Of Thread)
-      Dim Thread As Thread
-      Dim DataPipeEvent As DataPipeEvent
+      Dim Snapshot As AFValue
 
-      ' Determine the start of the current time interval. For this event source,
-      ' we only return at most one new event per attribute.
-      CurrentTimestamp = PreviousInterval(Now)
+      ' Iterate over all signed up attributes in this data pipe. For some
+      ' services signing up to many attributes, initialization might take some
+      ' time. After data is retrieved for all attributes for the first time,
+      ' only small amounts of new data are needed for all consequent
+      ' evaluations, greatly speeding them up.
+      For Each Attribute In MyBase.Signups
 
-      ' Only check for new events once per calculation interval.
-      If PreviousTimestamp < CurrentTimestamp Then
+        ' Check if we need to perform an action on this attribute. This is only
+        ' needed if the attribute is an instance of an object.
+        If Attribute IsNot Nothing Then
 
-        ' Iterate over all signed up attributes in this data pipe. For some
-        ' services signing up to many attributes, initialization might take some
-        ' time on the first calculation. After all data is retrieved for all
-        ' attributes for the first time, only small amounts of new data are
-        ' needed for all consequent evaluations, greatly speeding them up.
-        For Each Attribute In MyBase.Signups
+          Snapshot = Attribute.GetValue ' Retrieve latest value.
 
-          ' Check if we need to perform an action on this attribute. This is
-          ' only needed if the attribute is an instance of an object.
-          If Attribute IsNot Nothing Then
-
-            ' Start a new thread for each attribute, passing the attribute.
-            Threads.Add(New Thread(
-              New ParameterizedThreadStart(AddressOf RetrieveEvent)))
-            Threads(Threads.Count-1).Start(Attribute)
-
+          If PreviousSnapshots.ContainsKey(Attribute) Then
+            ' If newer, store snapshot timestamp as previous snapshot timestamp
+            ' for this attribute and publish new snapshot to the data pipe.
+            If Snapshot.Timestamp.LocalTime >
+              PreviousSnapshots.Item(Attribute) Then
+              PreviousSnapshots.Item(Attribute) = Snapshot.Timestamp.LocalTime
+              MyBase.PublishEvent(Attribute,
+                New AFDataPipeEvent(AFDataPipeAction.Add, Snapshot))
+            End If
+          Else
+            ' Store the initial previous snapshot timestamp for this attribute.
+            PreviousSnapshots.Add(Attribute, Snapshot.Timestamp.LocalTime)
           End If
 
-        Next
+        End If
 
-        ' Wait for all threads to finish.
-        For Each Thread In Threads
-          Thread.Join
-        Next
+      Next Attribute
 
-        ' Publish all events to the data pipe and clear the events list.
-        Monitor.Enter(DataPipeEvents) ' Lock
-        Try
-          For Each DataPipeEvent In DataPipeEvents
-            MyBase.PublishEvent(DataPipeEvent.Attribute,
-              New AFDataPipeEvent(AFDataPipeAction.Add, DataPipeEvent.Value))
-          Next
-          DataPipeEvents.Clear
-        Finally
-          Monitor.Exit(DataPipeEvents)
-        End Try
-
-        ' Store current interval as previous interval.
-        PreviousTimestamp = CurrentTimestamp
-
-      End If
-
-      Return False
+      Return False ' Required.
 
     End Function
 
