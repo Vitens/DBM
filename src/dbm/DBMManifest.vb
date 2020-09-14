e1b093c6ab476f5d747e664fe3c4dbf3f4a0723e
Johan Fitié <jfitie@gmail.com>
2020-09-14 08:35:12 +0200

    Update version (#265)

--- src/dbm/DBMManifest.vb
+++ src/dbm/DBMManifest.vb
@@ -26,7 +26,7 @@ Option Strict
 ' returned using the DBM.Version function.
 
 
-<assembly:System.Reflection.AssemblyVersion("1.61.*")>
+<assembly:System.Reflection.AssemblyVersion("1.62.*")>
 
 <assembly:System.Reflection.AssemblyProduct("Dynamic Bandwidth Monitor")>
 
