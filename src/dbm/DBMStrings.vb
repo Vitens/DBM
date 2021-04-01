79b09e23c7ab06b4ed63d3f7feed1a17517a4fef
Johan Fitié <jfitie@gmail.com>
2021-04-01 06:25:27 +0200

    Fix string

--- src/dbm/DBMStrings.vb
+++ src/dbm/DBMStrings.vb
@@ -41,7 +41,7 @@ Namespace Vitens.DynamicBandwidthMonitor
     Public Const sPercentageFormat As String = "0.0%"
     Public Const sCsvComment As String = "# "
     Public Const sDeflatline As String =
-      "De-flatline (Factor {0:" & sNumberFormat & ")"
+      "De-flatline (Factor {0:" & sNumberFormat & "})"
     Public Const sStatisticsInsufficientData As String =
       "Insufficient data for calculating model calibration metrics"
     Public Const sStatisticsBrief As String =
