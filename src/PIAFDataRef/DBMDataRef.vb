d0c850eed24b51900715b7bfde7ae6624290ee96
Johan Fitié <jfitie@gmail.com>
2021-04-02 13:23:34 +0200

    doc

--- src/PIAFDataRef/DBMDataRef.vb
+++ src/PIAFDataRef/DBMDataRef.vb
@@ -448,18 +448,19 @@ Namespace Vitens.DynamicBandwidthMonitor
                     ' flatline to the first forecast to be inserted, since the
                     ' value before the flatline will now propagate until the
                     ' first forecast.
-                    ' Step 1: Calculate previous weight of value before flatline
-                    '   and subtract this from the total forecast weight. This
-                    '   is the original weight which is valid for the previous
-                    '   value.
+                    ' Step 1: Calculate previous weight of value before the
+                    '   flatline and subtract this from the total forecast
+                    '   weight. This is the original weight which was valid for
+                    '   the previous value.
                     ForecastWeight -= WeightedValue(
                       Convert.ToDouble(Values.Item(iFL-1).Value),
                       Convert.ToDouble(Values.Item(iFL).Value),
                       Values.Item(iFL-1).Timestamp.LocalTime,
                       Values.Item(iFL).Timestamp.LocalTime, Stepped)
-                    ' Step 2: Calculate new weight of value before flatline and
-                    '   add this to the total forecast weight. This new weight
-                    '   has to be replaced by the original weight.
+                    ' Step 2: Calculate new weight of value before the flatline
+                    '   to first forecast and add this to the total forecast
+                    '   weight. This 'extra' weight is correctly included in the
+                    '   total forecast weight.
                     ForecastWeight += WeightedValue(
                       Convert.ToDouble(Values.Item(iFL-1).Value),
                       Result.ForecastItem.Forecast,
