9dc471bd44f612800bf9bcfe6cf41404790224d2
Johan Fitié <jfitie@gmail.com>
2021-04-15 13:27:40 +0200

    pct

--- src/dbm/DBMTests.vb
+++ src/dbm/DBMTests.vb
@@ -1392,8 +1392,8 @@ Namespace Vitens.DynamicBandwidthMonitor
 
       With Statistics(DBM.GetResults(InputPointDriver, CorrelationPoints,
         New DateTime(2016, 6, 1), New DateTime(2016, 7, 1)))
-        Return String.Format("SE {0:" & sNumberFormat & "} RE {1:" &
-          sNumberFormat & "} F {2:" & sNumberFormat & "}",
+        Return String.Format("SE {0:" & sPercentageFormat & "} RE {1:" &
+          sPercentageFormat & "} F {2:" & sPercentageFormat & "}",
           .SystematicError, .RandomError, .Fit)
       End With
 
