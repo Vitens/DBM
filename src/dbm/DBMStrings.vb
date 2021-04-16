ebd1ec1345964fe0b80a81765050e8e80537e316
Johan Fitié <jfitie@gmail.com>
2021-04-16 07:40:02 +0200

    unicode arrows

--- src/dbm/DBMStrings.vb
+++ src/dbm/DBMStrings.vb
@@ -49,10 +49,10 @@ Namespace Vitens.DynamicBandwidthMonitor
       "Random error {3:" & sPercentageFormat & "}; " &
       "Fit {4:" & sPercentageFormat & "})"
     Public Const sQualityTests As String =
-      "SE {0:" & sPercentageFormat & "}|{1:" & sPercentageFormat & "}|{2:" &
-        sPercentageFormat & "}; RE {3:" & sPercentageFormat & "}|{4:" &
-        sPercentageFormat & "}|{5:" & sPercentageFormat & "}; F {6:" &
-        sPercentageFormat & "}|{7:" & sPercentageFormat & "}|{8:" &
+      "SE {0:" & sPercentageFormat & "}↓{1:" & sPercentageFormat & "}↑{2:" &
+        sPercentageFormat & "}; RE {3:" & sPercentageFormat & "}↓{4:" &
+        sPercentageFormat & "}↑{5:" & sPercentageFormat & "}; F {6:" &
+        sPercentageFormat & "}↓{7:" & sPercentageFormat & "}↑{8:" &
         sPercentageFormat & "}"
     Public Const sForecastFactorAnnotation As String =
       sFactor & " {0:" & sNumberFormat & "}"
