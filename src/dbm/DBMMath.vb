f9c8797e44a492138953d594ed902ed8aa8ecb5c
Johan Fitié <jfitie@gmail.com>
2021-04-18 11:24:03 +0200

    stdev

--- src/dbm/DBMMath.vb
+++ src/dbm/DBMMath.vb
@@ -236,6 +236,32 @@ Namespace Vitens.DynamicBandwidthMonitor
     End Function
 
 
+    Public Shared Function StDev(Values() As Double) As Double
+
+      ' In statistics, the standard deviation is a measure of the amount of
+      ' variation or dispersion of a set of values. A low standard deviation
+      ' indicates that the values tend to be close to the mean (also called the
+      ' expected value) of the set, while a high standard deviation indicates
+      ' that the values are spread out over a wider range.
+
+      Dim MeanValues, Value As Double
+      Dim Count As Integer
+
+      MeanValues = Mean(Values)
+
+      For Each Value In Values
+        If Not IsNaN(Value) Then
+          StDev += (Value-MeanValues)^2
+          Count += 1
+        End If
+      Next
+      StDev /= Count
+
+      Return StDev
+
+    End Function
+
+
     Public Shared Function AbsoluteDeviation(Values() As Double,
       From As Double) As Double()
 
