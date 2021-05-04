8aa882dcd7f6b6c2c60a4f3ff47e7431e0eebab2
Johan Fitié <jfitie@gmail.com>
2021-05-04 07:38:25 +0200

    ut

--- src/dbm/DBMInfo.vb
+++ src/dbm/DBMInfo.vb
@@ -137,7 +137,7 @@ Namespace Vitens.DynamicBandwidthMonitor
       Dim QTResult As String
 
       Timer = Now
-'     RunUnitTests
+      RunUnitTests
       UTDurationMs = (Now.Ticks-Timer.Ticks)/TicksPerMillisecond
 
       Timer = Now
