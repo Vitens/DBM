e5fb4c6b1bc230fe43ebdc1e94b26838562eace7
Johan Fitie <jfitie@gmail.com>
2021-11-26 08:07:09 +0000

    Try fix

--- src/dbm/DBMPointDriverTestModel.vb
+++ src/dbm/DBMPointDriverTestModel.vb
@@ -727,8 +727,8 @@ Namespace Vitens.DynamicBandwidthMonitor
         ' Interpolate between two data points using weight.
         DataStore.AddData(StartTimestamp,
           Lerp(HourlyTimeSeriesData(DataPoint),
-          HourlyTimeSeriesData(DataPoint+1), 1-Weight)
-          Mod HourlyTimeSeriesData.Length))
+          HourlyTimeSeriesData(DataPoint+1), Weight) Mod
+          HourlyTimeSeriesData.Length))
 
         StartTimestamp =
           StartTimestamp.AddSeconds(CalculationInterval) ' Next interval.
